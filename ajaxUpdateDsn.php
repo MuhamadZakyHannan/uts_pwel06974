9be46e40d8ebac73e2e9f9e9f30e9f4e049a9276
zaky06974 <146085868+MuhamadZakyHannan@users.noreply.github.com>
2025-03-23 21:01:44 +0700

    added user

--- ajaxUpdateDsn.php
+++ ajaxUpdateDsn.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Sistem Informasi Akademik::Daftar Mahasiswa</title>
+		<title>Sistem Informasi Akademik::Daftar Dosen</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="bootstrap533/css/bootstrap.css">
